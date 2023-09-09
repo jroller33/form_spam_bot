36d072692110b5bc08e73ee190765c1df015d9ba
John Roller <jtroller33@gmail.com>
2023-09-09 16:11:03 -0500

    added kill py

--- kill_py.ps1
+++ kill_py.ps1
@@ -1,3 +1,4 @@
+# sometimes this bot can get hung up and start causing errors. If that happens, run this powershell script, which kills all Python processes and will also kill the bot
+
 taskkill /im python.exe /F
 taskkill /im python3.9.exe /F
-
