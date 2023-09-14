76f09425cd399e2d680b6c9e7f69d3f0f4ecc22d
John Roller <jtroller33@gmail.com>
2023-09-13 20:47:49 -0500

    about to test run alumni bot

--- alumni_form_bot.py
+++ alumni_form_bot.py
@@ -11,40 +11,51 @@ from names import first_name_list, last_name_list
 from hidden import alumni_form_url
 
 run_count = 0
+max_loop = random.choice(range(50,100))
+rand_run_id = random.choice(range(1,10000))
 
 try:
-    while run_count < 1:      # run_count starts at 0 and goes up to 99
+    while run_count < 1:      # run_count starts at 0 and is incremented at the end of this loop
+    # while run_count < max_loop:      # run_count starts at 0 and is incremented at the end of this loop
 
-        print(f"[***] Starting new ALUMNI run at run_count: {run_count}")
-
-        now = datetime.now()
-        timestamp_str = f'{now:%m.%d.%Y_%H.%M.%S}'
-        current_date = f'{now:%m.%d.%Y}'
+        start_now = datetime.now()
+        start_timestamp_str = f'{start_now:%H.%M.%S_%m.%d.%Y}'
+        current_date = f'{start_now:%m.%d.%Y}'
+        print(f"[***] Starting new ALUMNI run at run_count: {run_count} [{start_timestamp_str}]")
 
         # initialize the variables. every time this loop runs they will have different values
         first_name = random.choice(first_name_list)
         last_name = random.choice(last_name_list)
         email = generate_email(first_name, last_name)
         phone = generate_phone()
+        # history_choice = random.choice(range(1,3))    
+        history_choice = 1
 
-        history_choice = random.choice(range(2))
-        neg_history_choice = (2 - history_choice)
 
-        print(f"[*] {first_name},{last_name} {email} {phone}")
+
+        print(f"[*] {first_name}, {last_name} - HC:{history_choice} E:{email} P:{phone}")
 
 
         time.sleep(5)       # start new incognito Chrome window
         subprocess.Popen(f"start chrome /incognito {alumni_form_url}", shell=True)    # opens a new incognito Chrome window
         time.sleep(5)       # let the site finish loading
         pyautogui.getActiveWindow().maximize()  # maximize Chrome window
-        time.sleep(0.5)     
+        time.sleep(0.5)
+
 
         for tab in range(17):
             tab_sleep()
 
-        for choice in range(history_choice):
-            tab_rand_sleep()
-        space_rand_sleep()
+        if history_choice == 1:
+            tab_range = 19
+            space_rand_sleep()
+        elif history_choice == 2:
+            tab_range = 18
+            down_rand_sleep()
+        else:
+            print("!*!*!*!  ERROR IN HISTORY CHOICE  !*!*!*!*!")
+
+        print(f"[*] Tab Range = {tab_range}")
 
         tab_rand_sleep()
         tab_rand_sleep()
@@ -58,48 +69,28 @@ try:
 
         write_rand_sleep(email)
 
-        for tab in range(21):
-            tab_rand_sleep()        
+        tab_rand_sleep()
+        tab_rand_sleep()
+        write_rand_sleep(phone)
 
-        enter_rand_sleep()          # SUBMIT
-        time.sleep(random.uniform(2, 7))
+        for tab in range(tab_range):           # 18 for HC 2, 19 for HC 1
+            tab_rand_sleep()
 
-        # write_rand_sleep(email)
+        enter_rand_sleep()
 
-        # tab_rand_sleep()
-        # write_rand_sleep(full_name)
-
-        # tab_rand_sleep()
-        # write_rand_sleep(phone)
-
-        # tab_rand_sleep()
-        # space_rand_sleep()
-
-        # for tab in range(5):
-        #     tab_rand_sleep()
-
-        # for choice in range(event_choice):
-        #     tab_rand_sleep()
-        # space_rand_sleep()
-
-        # for tab in range(neg_event_choice):
-        #     tab_rand_sleep()
-        # space_rand_sleep()   # selects first selection of 'Committees' (PA Leadership)
-
-        # Evening of the arts worked
+        time.sleep(5)      # wait for the form to submit
+        pyautogui.getActiveWindow().close()  # close the Chrome window
 
 
+        end_now = datetime.now()
+        end_timestamp_str = f'{end_now:%H.%M.%S_%m.%d.%Y}'
+        print(f"[*] This run: {run_count} has finished at [{end_timestamp_str}]\n\n")
 
-
-
-
-
-
-        # time.sleep(10)      # wait for the form to submit
-        # pyautogui.getActiveWindow().close()  # close the Chrome window
-
-        print(f"[*] This run: {run_count} has finished.\n\n\n")
+        log = open(f"ALUMNI_BOT_{current_date}_{rand_run_id}", 'a')
+        log.write(f"Start:[{start_timestamp_str}] - End:[{end_timestamp_str}]\t {first_name}, {last_name} - E:{email}\t P:{phone}\t HC:{history_choice} TR:{tab_range}\t (if HC=1, TR=19. if HC=2, TR=18)\n\n")
+        log.close()
+        
         run_count += 1
 
-except KeyboardInterrupt:
+except KeyboardInterrupt:       # press CTRL-C to exit while the bot is running
     print(f"[!] Script exited at run_count: {run_count}")
--- scratch.py
+++ scratch.py
@@ -15,27 +15,44 @@ from custom_functions import generate_random_date
 from names import first_name_list
 from names import last_name_list
 
+
+rand_run_id = random.choice(range(1,1000))
+
+# for x in range(500):
+#     history_choice = random.choice(range(1,3))
+    
+#     if history_choice == 1:
+#         print("***** HC = 1 *****")
+
+#     elif history_choice == 2:
+#         print("***** HC = 2 *****")
+
+#     else:
+#         print("!*!*!*!  ERROR IN HISTORY CHOICE  !*!*!*!*!")
+
 run_count = 0
 
-while run_count < 100:
+while run_count < 20:
     now = datetime.now()
     timestamp_str = f'{now:%m.%d.%Y_%H.%M.%S}'
     current_date = f'{now:%m.%d.%Y}'
 
-    child_first_name = random.choice(first_name_list)
-    parent_first_name = random.choice(first_name_list)
+    first_name = random.choice(first_name_list)
     last_name = random.choice(last_name_list)
-    gender = random.choice(range(1, 4))
-    child_DOB = generate_random_date("child")
-    applying_for_year = random.choice(range(1,3))
-    applying_for_grade = random.choice(range(1,13))
-    relationship = random.choice(range(1, 3))
-    email = generate_email(parent_first_name, last_name)
+    email = generate_email(first_name, last_name)
     phone = generate_phone()
+    history_choice = random.choice(range(1,3)) 
+    
+    if history_choice == 1:
+        tab_range = 19
+    elif history_choice == 2:
+        tab_range = 18
+    else:
+        print("!*!*!*!  ERROR IN HISTORY CHOICE  !*!*!*!*!")
 
 
-    log = open(current_date, 'a')
-    log.write(f"[{timestamp_str}]\t C:{child_first_name},{last_name}\t P:{parent_first_name}\t {email}-{phone}\t DOB:{child_DOB} Gender:{gender}\t Y/G:{applying_for_year}-{applying_for_grade} Rel:{relationship}\n\n")
+    log = open(f"ALUMNI_BOT_{current_date}_{rand_run_id}", 'a')
+    log.write(f"[{timestamp_str}]\t {first_name}, {last_name} - E:{email}\t P:{phone}\t HC:{history_choice} TR:{tab_range}\t (if HC=1, TR=19. if HC=2, TR=18)\n\n")
 
     log.close()
     
